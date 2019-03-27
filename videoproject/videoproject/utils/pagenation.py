a33513cfc66aaaa3b69fa82f09670f9e3c6962d9
PeterMBP <peterpz0428@163.com>
2019-03-27 18:44:25 +0800

    分页

---
+++ videoproject/videoproject/utils/pagenation.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'peter'
+__mtime__ = '2019-03-27'
+# Follow the master,become a master.
+             ┏┓       ┏┓
+            ┏┛┻━━━━━━━┛┻┓
+            ┃    ☃      ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃ 神兽保佑     ┣┓
+              ┃　永无BUG！   ┏┛
+              ┗┓┓┏━━━┳┓┏━━━┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+
+
+def get_page_list(paginator, page):
+
+    """
+    分页逻辑
+    if 页数>=10:
+        当前页<=5时，起始页为1
+        当前页>(总页数-5)时，起始页为(总页数-9)
+        其他情况 起始页为(当前页-5)
+
+    举例：
+    假设一共16页
+    情况1: 当前页==5  则页码列表为[1,2,3,4,5,6,7,8,9,10]
+    情况2: 当前页==8  则页码列表为[3,4,5,6,7,8,9,10,11,12]
+    情况3: 当前页==15 则页码列表为[7,8,9,10,11,12,13,14,15,16]
+    """
+
+    page_list = []
+
+    if paginator.num_pages > 10:
+        if page.number <= 5:
+            start_page = 1
+        elif page.number > paginator.num_pages - 5:
+            start_page = paginator.num_pages - 9
+        else:
+            start_page = page.number - 5
+
+        for i in range(start_page, start_page + 10):
+            page_list.append(i)
+    else:
+        for i in range(1, paginator.num_pages + 1):
+            page_list.append(i)
+
+    return page_list
