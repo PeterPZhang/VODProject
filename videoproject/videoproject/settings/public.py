6da6a7744bb4a63552674c7b6fd51960d01f6c78
PeterMBP <peterpz0428@163.com>
2019-03-28 17:11:53 +0800

    增加公有配置文件

---
+++ videoproject/videoproject/settings/public.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'peter'
+__mtime__ = '2019-03-28'
+# Follow the master,become a master.
+             ┏┓       ┏┓
+            ┏┛┻━━━━━━━┛┻┓
+            ┃    ☃      ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃ 神兽保佑     ┣┓
+              ┃　永无BUG！   ┏┛
+              ┗┓┓┏━━━┳┓┏━━━┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+AUTH_USER_MODEL = 'users.User'
