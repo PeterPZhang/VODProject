dd38b9dd492a52e7cbaeb40d08fd1a2137625143
PeterMBP <peterpz0428@163.com>
2019-04-11 18:24:20 +0800

    增加公有方法工具限定ajax请求

---
+++ videoproject/videoproject/utils/common.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'peter'
+__mtime__ = '2019-04-11'
+# Follow the master,become a master.
+             ┏┓       ┏┓
+            ┏┛┻━━━━━━━┛┻┓
+            ┃    ☃      ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃ 神兽保佑     ┣┓
+              ┃　永无BUG！   ┏┛
+              ┗┓┓┏━━━┳┓┏━━━┛
+               ┃┫┫   ┃┫┫
+               ┗┻┛   ┗┻┛
+"""
+from django.http import HttpResponseBadRequest, HttpResponse
+
+
+# @ajax_required验证request必须是ajax
+def ajax_required(f):
+    """Not a mixin, but a nice decorator to validate than a request is AJAX"""
+    def wrap(request, *args, **kwargs):
+        if not request.is_ajax():
+            return HttpResponseBadRequest()
+
+        return f(request, *args, **kwargs)
+
+    wrap.__doc__ = f.__doc__
+    wrap.__name__ = f.__name__
+    return wrap
