8237cc402162582d32fbd1cb06c7b7eaf8b8997d
PeterMBP <peterpz0428@163.com>
2019-04-28 17:16:30 +0800

    上传视频模型

--- videoproject/videoproject/apps/myadmin/models.py
+++ videoproject/videoproject/apps/myadmin/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# Create your models here.
+from chunked_upload.models import ChunkedUpload
+
 
 # Create your models here.
+class MyChunkedUpload(ChunkedUpload):
+    pass
+
+
+# Override the default ChunkedUpload to make the `user` field nullable
+MyChunkedUpload._meta.get_field('user').null = True
