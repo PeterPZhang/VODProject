2e148c69ae579f969878a944143e1f4af084f04e
PeterMBP <peterpz0428@163.com>
2019-04-30 15:03:52 +0800

    注册管理员应用

--- videoproject/videoproject/settings/dev.py
+++ videoproject/videoproject/settings/dev.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'sorl.thumbnail',
-    'users',
+    'chunked_upload',
     'video',
-    'video.templatetags',
+    'users',
+    'myadmin',
     'comment',
 ]
 
