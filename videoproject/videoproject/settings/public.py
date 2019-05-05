1189b38deb5e29b0390d71768d82eafbf94883d7
PeterMBP <peterpz0428@163.com>
2019-05-05 17:06:35 +0800

    邮箱配置

--- videoproject/videoproject/settings/public.py
+++ videoproject/videoproject/settings/public.py
@@ -17,3 +17,13 @@ __mtime__ = '2019-03-28'
                ┗┻┛   ┗┻┛
 """
 AUTH_USER_MODEL = 'users.User'
+SITE_URL = 'your website'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# 邮件配置
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'peterpz0428@163.com'  # 邮箱名称
+EMAIL_HOST_PASSWORD = 'your pwd'  # 邮箱密码
